fba8595e803f8bb4be3a70bfa12f23bf0f0bb7ec
YJ <2139@kangwon.ac.kr>
2018-11-29 22:46:41 +0900

    ass1

--- imgproc.cpp
+++ imgproc.cpp
@@ -8,9 +8,8 @@ namespace IPCVL {
 			for (int y = 0; y < inputMat.rows; y++) {
 				for (int x = 0; x < inputMat.cols; x++) {
 					// Todo : histogram을 쌓습니다. 
-
 					/** your code here! **/
-
+					histogram[inputMat.at<uchar>(y, x)]++;
 					// hint 1 : for loop 를 이용해서 cv::Mat 순회 시 (1채널의 경우) 
 					// inputMat.at<uchar>(y, x)와 같이 데이터에 접근할 수 있습니다. 
 				}
@@ -19,10 +18,10 @@ namespace IPCVL {
 		void backprojectHistogram(cv::InputArray src_hsv, cv::InputArray face_hsv, cv::OutputArray dst) {
 			cv::Mat srcMat = src_hsv.getMat();
 			cv::Mat faceMat = face_hsv.getMat();
-			dst.create(srcMat.size(), CV_64FC1);
+			dst.create(srcMat.size(), CV_8UC1);
 			cv::Mat outputProb = dst.getMat();
 			outputProb.setTo(cv::Scalar(0.));
-
+			
 			double model_hist[64][64] = { { 0., } };
 			double input_hist[64][64] = { { 0., } };
 
@@ -30,12 +29,18 @@ namespace IPCVL {
 			calcHist_hs(srcMat, input_hist);
 			calcHist_hs(faceMat, model_hist);
 
+			std::vector<cv::Mat> channels;
+			split(srcMat, channels);
+			cv::Mat mat_h = channels[0];
+			cv::Mat mat_s = channels[1];
+
 			for (int y = 0; y < srcMat.rows; y++) {
 				for (int x = 0; x < srcMat.cols; x++) {
 					// Todo : 양자화된 h,s 값을 얻고 histogram에 값을 더합니다. 
-
+					int h = UTIL::quantize(mat_h.at<uchar>(y, x));
+					int s = UTIL::quantize(mat_s.at<uchar>(y, x));
 					/** your code here! **/
-
+					outputProb.at<uchar>(y, x) = UTIL::h_r(model_hist, input_hist, h, s) * 255;
 					// hint 1 : UTIL::quantize()를 이용해서 srtMat의 값을 양자화합니다. 
 					// hint 2 : UTIL::h_r() 함수를 이용해서 outputPorb 값을 계산합니다. 
 				}
@@ -53,9 +58,10 @@ namespace IPCVL {
 			for (int y = 0; y < hsv.rows; y++) {
 				for (int x = 0; x < hsv.cols; x++) {
 					// Todo : 양자화된 h,s 값을 얻고 histogram에 값을 더합니다. 
-
+					int h = UTIL::quantize(mat_h.at<uchar>(y, x));
+					int s = UTIL::quantize(mat_s.at<uchar>(y, x));
 					/** your code here! **/
-
+					histogram[h][s]++;
 					// hint 1 : 양자화 시 UTIL::quantize() 함수를 이용해서 mat_h, mat_s의 값을 양자화시킵니다. 
 				}
 			}
@@ -65,6 +71,7 @@ namespace IPCVL {
 				for (int i = 0; i < 64; i++) {
 					// Todo : histogram에 있는 값들을 순회하며 (hsv.rows * hsv.cols)으로 정규화합니다. 
 					/** your code here! **/
+					histogram[j][i] /= (hsv.rows * hsv.cols);
 				}
 			}
 		}
