54c0b580ddbe99e3c6453d740abc59abc404c954
SHCHOI2220 <shchoi2220>
2018-12-04 16:32:22 +0900

    shchoi_week1_complete

--- assignment1_2.cpp
+++ assignment1_2.cpp
@@ -6,12 +6,12 @@
 #include "imgproc.h"
 
 int main() {
-	cv::Mat input = cv::imread("samples/sample1.jpg");
+	cv::Mat input = cv::imread("samples/sample1.bmp");
 	cv::Mat face = cv::imread("samples/face.png");
 
 	cv::Mat src_hsv, face_hsv;
-	cvtColor(input, src_hsv, cv::COLOR_BGR2HSV);
-	cvtColor(face, face_hsv, cv::COLOR_BGR2HSV);
+	cvtColor(input, src_hsv, CV_8UC3);
+	cvtColor(face, face_hsv, CV_8UC3);
 
 	cv::Mat output;
 	// Todo : imageproc.cpp에 있는 backprojectHistogram 함수를 작성하세요
